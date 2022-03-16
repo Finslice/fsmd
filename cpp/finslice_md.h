91319b3418763bc30d2c135ac3d626a93d1c531a
Finslice <devs@finslice.com>
2022-03-15 22:45:28 -0400

    Smol Fix

--- cpp/finslice_md.h
+++ cpp/finslice_md.h
@@ -17,27 +17,41 @@ private:
 public:
   MDMessage() = default;
   MDMessage(char *buffer) : _buffer(buffer) {}
+
+  static constexpr size_t channelHdrLength() noexcept { return 12; }
+
   uint64_t getChannelTimestamp() {
     uint64_t val;
     std::memcpy(&val, _buffer, sizeof(std::uint64_t));
     return val;
   }
+
+  void setChannelTimestamp(const uint64_t value) { std::memcpy(_buffer, &value, sizeof(uint64_t)); }
+
   uint32_t getChannelSequence() {
     uint32_t val;
-    std::memcpy(&val, _buffer, sizeof(std::uint32_t));
+    std::memcpy(&val, _buffer + 8, sizeof(std::uint32_t));
     return val;
   }
+
+  void setChannelSequence(const uint32_t value) {
+    std::memcpy(_buffer + 8, &value, sizeof(uint32_t));
+  }
+
   MessageHeader getMsgHeader() {
-    MessageHeader hdr(_buffer + 12, 8);
+    MessageHeader hdr(_buffer + channelHdrLength(), 8);
     return hdr;
   }
+
   template <typename T> T getEvent() {
-    T event(_buffer + 20, 192);
+    T event(_buffer + channelHdrLength() + MessageHeader::encodedLength(), 172);
     // auto hdr = getMsgHeader();
     // event.wrapForDecode(_buffer + 12, hdr.encodedLength(), hdr.blockLength(), hdr.version(),
     // 192);
     return event;
   }
+
+  char *getSBEBuf() { return _buffer + 12; }
 
   template <typename CharT, typename Traits>
   friend std::basic_ostream<CharT, Traits> &operator<<(std::basic_ostream<CharT, Traits> &builder,
@@ -69,4 +83,21 @@ public:
     return builder;
   }
 };
+
+struct MDBuffer {
+  size_t len;
+  char buffer[192];
+  void clean() { std::memset(buffer, 0, len); }
+  MDMessage getMD() {
+    MDMessage md(buffer);
+    return md;
+  }
+  template <typename T> T getEvent() {
+    T event(buffer + MDMessage::channelHdrLength() + MessageHeader::encodedLength(), 172);
+    return event;
+  }
+};
+
+using MDBufferPtr = std::unique_ptr<MDBuffer>;
+
 } // namespace finslice_md
